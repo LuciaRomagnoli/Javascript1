f0770e50985630f94969f63e1b0a5c95474ec3b1
LuciaRomagnoli <lucia.romagnoli88@gmail.com>
2024-10-07 13:09:45 +0200

    primi script

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 console.log('Hello World!')
+console.log(10 + 5)
+//varabile://
+let myDog
+myDog = 'Pluto'
+let myDog = 'Pluto'
+let age = 25
+//commento
+console.log('age prima', age) //senza apici vale il valore
+age = 50
+console.log('age dopo', age)
